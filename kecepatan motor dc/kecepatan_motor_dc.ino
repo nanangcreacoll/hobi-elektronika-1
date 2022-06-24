79b109bcbb8967be3b30c0c84f441b029d8f3602
Nanang Muhamad <106458301+nanangcreacoll@users.noreply.github.com>
2022-06-25 01:53:46 +0700

    Add files via upload

--- kecepatan motor dc/kecepatan_motor_dc.ino
+++ kecepatan motor dc/kecepatan_motor_dc.ino
@@ -1,1 +1,38 @@
+#define potensio A0
+#define ENA 3
+#define IN1 0
+#define IN2 1
+#define led 7
+#define button_hidup 13
+#define button_mati 12
 
+int kecepatan=0;
+
+void setup() {
+  pinMode(potensio,INPUT);
+  pinMode(button_hidup,INPUT_PULLUP);
+  pinMode(button_mati,INPUT_PULLUP);
+  pinMode(led,OUTPUT);
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+  pinMode(ENA,OUTPUT);
+  analogWrite(ENA,0);
+  delay(500);
+}
+
+
+void loop() {
+  kecepatan=analogRead(potensio);
+  kecepatan=map(kecepatan,0,1023,0,255);
+  
+  if(digitalRead(button_hidup)==LOW){
+    digitalWrite(IN1,HIGH);
+    digitalWrite(IN2,LOW);
+    analogWrite(ENA,kecepatan);
+    digitalWrite(led,HIGH);
+  }
+  else if(digitalRead(button_mati)==LOW){
+    analogWrite(ENA,0);
+    digitalWrite(led,LOW);
+  }
+}
